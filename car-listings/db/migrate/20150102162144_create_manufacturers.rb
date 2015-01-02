5bbeb5ed3f6677b085bbe9b351b473ae00531283
Diego De Souza <a.t.dieps17@yahoo.com>
2015-01-02 11:30:30 -0500

    fixed missed null value in models

--- car-listings/db/migrate/20150102162144_create_manufacturers.rb
+++ car-listings/db/migrate/20150102162144_create_manufacturers.rb
@@ -1,8 +1,8 @@
 class CreateManufacturers < ActiveRecord::Migration
   def change
     create_table :manufacturers do |t|
-      t.string :manufacturer
-      t.string :country
+      t.string :manufacturer, null: false
+      t.string :country, null: false
 
       t.timestamps null: false
     end
